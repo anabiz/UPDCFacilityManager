6e18f26e49605f41c06baa6f17d078705ba723e8
Anthony Iwuji <anabizconcept9@gmail.com>
2023-02-15 19:00:35 +0100

    refactor and cleanup

--- src/Bootstrapper/UPDCFacilityManager.Bootstrapper/wwwroot/js/site.js
+++ src/Bootstrapper/UPDCFacilityManager.Bootstrapper/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+console.log('eeeeeeeeeeeeeeeeeeeeee')
+document.getElementById('test').addEventListener('click', (e) => {
+    console.log(e)
+    alert("hello world");
+    console.log("hello wor")
+})
