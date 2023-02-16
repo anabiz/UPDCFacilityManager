90469809f74e18bb6a9973a8627f8dd13f998338
Anthony Iwuji <anabizconcept9@gmail.com>
2023-02-16 13:47:56 +0100

    add service extension to Auth project

---
+++ Modules/Auth/UPDCFacilityManager.Modules.Auth.Core/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UPDCFacilityManager.Modules.Auth.Core.Data;
+
+namespace UPDCFacilityManager.Modules.Auth.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(config.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            return services;
+        }
+    }
+}
